6d5db49cb8ca703ca625e8e3975be483a978fd67
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-12 22:29:00 -0700

    EW HW07_ex10_06

--- HW07_ex10_06.py
+++ HW07_ex10_06.py
@@ -3,3 +3,30 @@
 # In your final submission: 
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+# Imports
+
+# Body
+def is_sorted(various_lists):
+	""" compares the list to the sorted version of the list. should this work if there are nested lists?
+	it doesn't. how would I make it do that???"""
+	sorted_list = sorted(various_lists)
+	if sorted_list == various_lists:
+		return True
+	else:
+		return False
+
+
+
+
+##############################################################################
+def main():
+
+    pass
+    # print is_sorted(['a', 'b', ['c','d']])
+    # print is_sorted(['c','d'])
+    # print is_sorted([[1], [2], [4,3], 9, 10])
+    # print is_sorted([[1],2,3,[4,5]])
+
+if __name__ == '__main__':
+    main()
