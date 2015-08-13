f1d9e2f8f87f34e5fd83888cdb3ea8e5938cf8af
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-12 22:02:36 -0700

    EW HW07_10_02

--- HW07_ex10_02.py
+++ HW07_ex10_02.py
@@ -5,3 +5,27 @@
 # In your final submission: 
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+# Imports
+
+# Body
+def capitalize_nested(nested_lists):
+	"""function flattens the nested list into a singled list, then joins the lists into a string
+	then returns the string with all uppercase then splits the string back into a list. but shoot
+	I just realized I'm supposed to return a new nested list and I don't know how to do that."""
+	flattened = [' '.join(item) if type(item) is list else item for item in nested_lists]
+	joined = ' '.join(flattened)
+	capitalized = joined.upper()
+	new_list = capitalized.split()
+	print new_list
+
+
+##############################################################################
+def main():
+
+    pass
+    # capitalize_nested(['apple', ['bear'], 'cat'])
+    # capitalize_nested(['reading', ['literacy', 'Literacy'], 'Reading'])
+
+if __name__ == '__main__':
+    main()
